7f592bc4a9ab5d5ca5c922836ec876cb665434f4
Matheus Morais <matheusmty@gmail.com>
2024-10-31 18:44:42 -0300

    chore: update Users and Addresses tables to set ID as auto-incrementable

---
+++ Migrations/20241031211758_edit_tables_Usuarios_Enderecos_EditColum_id_autoIncrimental.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AEC.Migrations
+{
+    /// <inheritdoc />
+    public partial class edit_tables_Usuarios_Enderecos_EditColum_id_autoIncrimental : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
