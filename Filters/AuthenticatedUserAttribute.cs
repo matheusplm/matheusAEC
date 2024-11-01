ce1815ddb702cb63e9ba3a35696b204b339f948a
Matheus Morais <matheusmty@gmail.com>
2024-11-01 17:17:13 -0300

    feat: Add AuthenticatedUserAttribute

---
+++ Filters/AuthenticatedUserAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace AEC.Filters
+{
+    public class AuthenticatedUserAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.HttpContext.Session.GetInt32("UserId") == null)
+            {
+                context.Result = new RedirectToActionResult("Index", "Login", null);
+                context.HttpContext.Session.SetString("ErrorMessage", "Você deve estar autenticado para acessar essa página.");
+            }
+            base.OnActionExecuting(context);
+        }
+    }
+}
